08dd30e5315000c648eac1f4ef60f1ae4e338250
Gintare Remeikiene <gin.remeikiene@gmail.com>
2021-04-16 02:02:19 +0300

    add input field validation

--- src/main/java/lt/tasks/rates/dto/CurrencyDto.java
+++ src/main/java/lt/tasks/rates/dto/CurrencyDto.java
@@ -2,27 +2,17 @@ package lt.tasks.rates.dto;
 
 import java.util.*;
 
+
 public class CurrencyDto {
-    // FIXME add validations
-    // NOTE range is just a list of dates
-    // Must be at least one
-    // MUST NOT BE EMPTY STRINGS
-    // CHECK WITH REGEX
+
     private TreeSet<String> dates;
-    // Base currency type
-    // Must exist
-    // MUST NOT BE EMPTY STRINGS
+
     private String baseCurrency;
-    // Currency codes to get data of
-    // Must be at least one
-    // MUST NOT BE EMPTY STRINGS
-    // CHECK WITH REGEX IF ONLY THREE LETTERS IN  CURRENCY CODE
+
     private List<String> targetCurrencies;
 
-    public CurrencyDto() {
-    }
-
     public CurrencyDto(String baseCurrency, List<String> targetCurrencies, TreeSet<String> dates) {
+        validate(baseCurrency, targetCurrencies, dates);
         this.dates = dates;
         this.baseCurrency = baseCurrency;
         this.targetCurrencies = targetCurrencies;
@@ -32,23 +22,31 @@ public class CurrencyDto {
         return dates;
     }
 
-    public void setDates(TreeSet<String> dates) {
-        this.dates = dates;
-    }
-
     public List<String> getTargetCurrencies() {
         return targetCurrencies;
-    }
-
-    public void setTargetCurrencies(List<String> targetCurrencies) {
-        this.targetCurrencies = targetCurrencies;
     }
 
     public String getBaseCurrency() {
         return baseCurrency;
     }
 
-    public void setBaseCurrency(String baseCurrency) {
-        this.baseCurrency = baseCurrency;
+    private void validate(String baseCurrency, List<String> targetCurrencies, TreeSet<String> dates) {
+        if (!baseCurrency.matches("^[A-Z]{2}")) {
+            throw new RuntimeException("Wrong base currency pattern.");
+        }
+
+        targetCurrencies.forEach(currency -> {
+            if(!currency.matches("^[A-Z]{3}")){
+                throw new RuntimeException("Wrong target currency pattern.");
+            }
+        });
+
+        dates.forEach(date -> {
+            if(!date.matches("^[0-9]{4}-[0-9]{2}-[0-9]{2}")){
+                throw new RuntimeException("Wrong date pattern.");
+            }
+        });
+
+
     }
 }
