bdf45fe55571bdf87d2e971b5fdd39f4f7c926a6
Gintare Remeikiene <gin.remeikiene@gmail.com>
2021-04-16 02:02:53 +0300

    update swagger docs

--- src/main/java/lt/tasks/rates/controller/CurrencyController.java
+++ src/main/java/lt/tasks/rates/controller/CurrencyController.java
@@ -1,6 +1,7 @@
 package lt.tasks.rates.controller;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lt.tasks.rates.dto.CurrencyDto;
 import lt.tasks.rates.service.CurrencyService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,10 +22,23 @@ public class CurrencyController {
     private CurrencyService currencyService;
 
     @GetMapping("/csv")
-    @ApiOperation(value = "Returns file containing currency changes", notes = "Returns file containing currency changes. Base Currency EU or LT. Data format YYYY-MM-DD. ")
-    public ResponseEntity getCurrencies(@RequestParam String baseCurrency, @RequestParam List<String> currencies, @RequestParam TreeSet<String> dates){
-        CurrencyDto currencyDto = new CurrencyDto(baseCurrency, currencies, dates);
+    @ApiOperation(value = "Returns file containing currency changes", notes = "Returns file containing currency ratio changes.")
+    public ResponseEntity getCurrencies(
+            @ApiParam(
+                    value = " Base Currency EU or LT",
+                    example = "EU",
+                    required = true)
+            @RequestParam String baseCurrency,
+            @ApiParam(
+                    value = "List of three letter currency codes e.g. USD",
+                    required = true)
+            @RequestParam List<String> currencies,
+            @ApiParam(
+                    value = "List of dates to get currency ratio for e.g. 2020-03-18",
+                    required = true)
+            @RequestParam TreeSet<String> dates){
         try {
+            CurrencyDto currencyDto = new CurrencyDto(baseCurrency, currencies, dates);
             InputStreamResource file = currencyService.getFile(currencyDto);
             return ResponseEntity.ok()
                     .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + "currencies.csv")
