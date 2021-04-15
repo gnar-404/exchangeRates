f33099b3f9ce7cbb9cb8ac66e0f689996a7dfc4e
Gintare Remeikiene <gin.remeikiene@gmail.com>
2021-04-16 02:29:26 +0300

    cleanup

--- src/main/java/lt/tasks/rates/service/CurrencyService.java
+++ src/main/java/lt/tasks/rates/service/CurrencyService.java
@@ -18,7 +18,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.SortedSet;
-import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 @Service
