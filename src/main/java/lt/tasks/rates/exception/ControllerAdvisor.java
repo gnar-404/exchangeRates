79aeb1e41161d94b5cbb106196ea7f0612262819
Gintare Remeikiene <gin.remeikiene@gmail.com>
2021-04-16 02:06:09 +0300

    code cleanup

--- src/main/java/lt/tasks/rates/exception/ControllerAdvisor.java
+++ src/main/java/lt/tasks/rates/exception/ControllerAdvisor.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.LinkedHashMap;
+
 import java.util.Map;
+
 
 
 @ControllerAdvice
